822ed11797ea2be12bba05d709caf99a1e9d9369
maximilien0405 <maximilien.zimmermann@ik.me>
2021-05-12 00:07:49 +0200

    edits on config & continue structure & design (more changes tomorow)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  ENABLE_PURGE: true
 };
 
 /*
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
-
 module.exports = {
+  mode: 'jit',
   purge: {
-      enabled: true,
-      content: [
-        './src/**/*.html',
-        './src/**/*.scss'
-      ]
-    },
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         white_light: "#F3F4F6",
-        grey_background_light: "#37393F",
-        grey_background_dark: "#202225",
-        grey_subtitle: "#AAAAAB",
+        gray_background_light: "#37393F",
+        gray_background_dark: "#202225",
+        gray_subtitle: "#AAAAAB",
         blue_border: "#CDF6FF",
         blue_button: "#12606D",
       },
