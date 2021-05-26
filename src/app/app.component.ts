50c522995ba329573c3148673187a249c46e9d33
Maximilien Zimmermann <maximilien.zimmermannn@gmail.com>
2021-05-26 17:32:43 +0200

    edit basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styles: ['']
 })
 export class AppComponent {
-  title = 'justrun-web';
+  url = "";
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.url = this.router.url;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
 }
