3277ae77fbad2749b24e1f43eb58a087973c7cce
Maximilien Zimmermann <maximilien.zimmermannn@gmail.com>
2021-05-27 17:26:41 +0200

    make language switch possible + other responsive fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,23 @@ import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 })
 export class AppComponent {
   url = "";
+  public lang: string = "FR";
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public translate: TranslateService) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.url = this.router.url;
       }
     });
+
+    translate.addLangs(['fr','en'])
+    translate.setDefaultLang('fr')
+    const browserlang = translate.getBrowserLang();
+    translate.use(browserlang.match(/en|fr/) ? browserlang: "en");
+  }
+
+  public changeLang(lang: string) {
+    this.lang = lang;
   }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TheGameComponent } from './the-game/the-game.component';
@@ -8,17 +10,30 @@ import { MediasComponent } from './medias/medias.component';
 import { OurTeamComponent } from './our-team/our-team.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+
 @NgModule({
   declarations: [
     AppComponent,
     TheGameComponent,
     MediasComponent,
     OurTeamComponent,
-    ContactUsComponent
+    ContactUsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
