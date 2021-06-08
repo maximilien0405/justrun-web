4e826f55d56f4aff6582bcf9647d7615daa44719
maximilien0405 <maximilien.zimmermann@ik.me>
2021-06-08 22:51:29 +0200

    start of team page

---
+++ src/app/common/models/profile.model.ts
@@ -0,0 +1,12 @@
+export interface Profile {
+  userName: string,
+  role: string,
+  pdp: string,
+  presentation: string,
+  socialTwitter: string,
+  socialInsta: string,
+  socialLinkedin: string,
+  socialFacebook: string,
+  socialEmail: string,
+  socialWebsite: string,
+}
---
+++ src/app/profile-list.ts
@@ -0,0 +1,16 @@
+import { Profile } from './common/models/profile.model'
+
+export const PROFILE: Profile[] = [
+    {
+      userName: "Maximilien Zimmermann",
+      role: "",
+      pdp: "",
+      presentation: "",
+      socialTwitter: "",
+      socialInsta: "",
+      socialLinkedin: "",
+      socialFacebook: "",
+      socialEmail: "",
+      socialWebsite: "",
+    },
+];
