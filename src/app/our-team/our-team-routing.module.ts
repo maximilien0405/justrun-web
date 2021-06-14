63f36486e9dd61b29c3d7778d6ae7b6f7e7e3c76
Maximilien Zimmermann <maximilien.zimmermannn@gmail.com>
2021-06-14 11:48:20 +0200

    end desktop menu design

--- src/app/our-team/our-team-routing.module.ts
+++ src/app/our-team/our-team-routing.module.ts
@@ -25,4 +25,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserSettingsRoutingModule { }
+export class OurTeamRoutingModule { }
