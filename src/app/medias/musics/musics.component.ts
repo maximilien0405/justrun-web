f22ef5429fb9778a4d23efb27f8199913a9d1eba
Maximilien Zimmermann <maximilien.zimmermannn@gmail.com>
2021-06-18 16:31:17 +0200

    start new aspect our team page

---
+++ src/app/medias/musics/musics.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-musics',
+  templateUrl: './musics.component.html'
+})
+export class MusicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
