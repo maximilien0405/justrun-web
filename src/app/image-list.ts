2b47ca7c5e6288c367a5e846f48e27ee4068dd2a
maximilien0405 <maximilien.zimmermann@ik.me>
2021-06-21 23:52:47 +0200

    f

--- src/app/image-list.ts
+++ src/app/image-list.ts
@@ -3,13 +3,13 @@ import { Image } from './common/models/image.model'
 export const IMAGEList1: Image[] = [
     {
       image: "../../../assets/img/img1.png",
-      descriptionFR: "FR----------Lorem ipsum dolor sit amet",
-      descriptionEN: "EN----------Lorem ipsum dolor sit amet",
+      descriptionFR: "FR----------Image 1",
+      descriptionEN: "EN----------Image 1",
     },
     {
       image: "../../../assets/img/img2.png",
-      descriptionFR: "FR---------Lorem ipsum dolor sit amet",
-      descriptionEN: "EN---------Lorem ipsum dolor sit amet",
+      descriptionFR: "FR---------Image 2",
+      descriptionEN: "EN---------Image 2",
     },
     {
       image: "../../../assets/img/img3.png",
@@ -61,7 +61,7 @@ export const IMAGEList1: Image[] = [
       descriptionFR: "FR---------Lorem ipsum dolor sit amet",
       descriptionEN: "EN---------Lorem ipsum dolor sit amet",
     },
-  
+
 ];
 
 export const IMAGEList2: Image[] = [
