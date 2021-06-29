726b8212006c59f5bfb4a70332ec44fc5bcb093f
Maximilien Zimmermann <maximilien.zimmermannn@gmail.com>
2021-06-29 17:57:46 +0200

    replace all png images to jpg

--- src/app/image-list.ts
+++ src/app/image-list.ts
@@ -4,22 +4,22 @@ import { Video } from './common/models/video.model';
 
 export const IMAGEList1: Image[] = [
     {
-      image: "../../../assets/img/drawings/etage.png",
+      image: "../../../assets/img/drawings/etage.jpg",
       descriptionFR: "Aspect d'un étage",
       descriptionEN: "Aspect of a floor",
     },
     {
-      image: "../../../assets/img/drawings/frejel.png",
+      image: "../../../assets/img/drawings/frejel.jpg",
       descriptionFR: "Frejel avec tout son équipement",
       descriptionEN: "Frejel with all his equipment",
     },
     {
-      image: "../../../assets/img/drawings/mains.png",
+      image: "../../../assets/img/drawings/mains.jpg",
       descriptionFR: "Main robotique",
       descriptionEN: "Robotic hand",
     },
     {
-      image: "../../../assets/img/drawings/robot1.png",
+      image: "../../../assets/img/drawings/robot1.jpg",
       descriptionFR: "Croquis test du robot 'C-039'",
       descriptionEN: "Test sketch of the 'C-039' robot",
     },
@@ -34,7 +34,7 @@ export const IMAGEList1: Image[] = [
       descriptionEN: "Research of the aspect of the robot 'A-373'",
     },
     {
-      image: "../../../assets/img/drawings/wilson.png",
+      image: "../../../assets/img/drawings/wilson.jpg",
       descriptionFR: "Croquis visage de Wilson",
       descriptionEN: "Sketch of Wilson's face",
     },
@@ -44,27 +44,27 @@ export const IMAGEList1: Image[] = [
       descriptionEN: "Robot test",
     },
     {
-      image: "../../../assets/img/drawings/robot7.png",
+      image: "../../../assets/img/drawings/robot7.jpg",
       descriptionFR: "Robot 'S-039'",
       descriptionEN: "Robot 'S-039'",
     },
     {
-      image: "../../../assets/img/drawings/robot8.png",
+      image: "../../../assets/img/drawings/robot8.jpg",
       descriptionFR: "Détails buste du robot 'A-402'",
       descriptionEN: "Chest details of robot 'A-402",
     },
     {
-      image: "../../../assets/img/drawings/tours.png",
+      image: "../../../assets/img/drawings/tours.jpg",
       descriptionFR: "Croquis détaillé de la tour AntechCorp",
       descriptionEN: "Detailed sketch of the AntechCorp tower",
     },
     {
-      image: "../../../assets/img/drawings/ville.png",
+      image: "../../../assets/img/drawings/ville.jpg",
       descriptionFR: "Ville 1",
       descriptionEN: "City 1",
     },
     {
-      image: "../../../assets/img/drawings/ville2.png",
+      image: "../../../assets/img/drawings/ville2.jpg",
       descriptionFR: "Ville 2",
       descriptionEN: "City 2",
     },
@@ -77,46 +77,46 @@ export const IMAGEList1: Image[] = [
 
 export const IMAGEList2: Image[] = [
   {
-    image: "../../../assets/img/models/operation.png",
+    image: "../../../assets/img/models/operation.jpg",
     descriptionFR: "Table d'opération - Vue globale",
     descriptionEN: "Operating table - Global view",
   },    {
-    image: "../../../assets/img/models/operation2.png",
+    image: "../../../assets/img/models/operation2.jpg",
     descriptionFR: "Table d'opération - Vue rapprochée",
     descriptionEN: "Operating table - Close-up view",
   },
   {
-    image: "../../../assets/img/models/blaster.png",
+    image: "../../../assets/img/models/blaster.jpg",
     descriptionFR: "Arme de Wilson - Version blaster",
     descriptionEN: "Wilson's weapon - Blaster version",
   },
 
   {
-    image: "../../../assets/img/models/pompe.png",
+    image: "../../../assets/img/models/pompe.jpg",
     descriptionFR: "Arme de Wilson - Version fusil à pompe",
     descriptionEN: "Wilson's Weapon - Shotgun version",
   },
   {
-    image: "../../../assets/img/models/robot1.png",
+    image: "../../../assets/img/models/robot1.jpg",
     descriptionFR: "Robot 'S-019'",
     descriptionEN: "S-019' robot",
   },    {
-    image: "../../../assets/img/models/robot2.png",
+    image: "../../../assets/img/models/robot2.jpg",
     descriptionFR: "Robot 'A-373'",
     descriptionEN: "A-373' robot",
   },
   {
-    image: "../../../assets/img/models/robot3.png",
+    image: "../../../assets/img/models/robot3.jpg",
     descriptionFR: "Robot 'A-402' - Version militaire",
     descriptionEN: "'A-402' robot - Military version",
   },
   {
-    image: "../../../assets/img/models/frejel.png",
+    image: "../../../assets/img/models/frejel.jpg",
     descriptionFR: "Frejel avec son costume",
     descriptionEN: "Frejel with his suit",
   },
    {
-    image: "../../../assets/img/models/box.png",
+    image: "../../../assets/img/models/box.jpg",
     descriptionFR: "Boîtes en carton pour l'entrepôt",
     descriptionEN: "Cardboard boxes for storage",
   }
@@ -125,19 +125,19 @@ export const IMAGEList2: Image[] = [
 export const IMAGEList3: Video[] = [
   {
     image: "https://www.youtube.com/embed/1aDbkW2zI_M",
-    cover: "../../../assets/cover/img1.png",
+    cover: "../../../assets/cover/img1.jpg",
     descriptionFR: "JustRun - Trailer GameCup 2021",
     descriptionEN: "JustRun - GameCup trailer 2021",
   },
   {
     image: "https://www.youtube.com/embed/BP_ta9sWS5A",
-    cover: "../../../assets/cover/img2.png",
+    cover: "../../../assets/cover/img2.jpg",
     descriptionFR: "JustRun - La tour",
     descriptionEN: "JustRun - The tower",
   },
   {
     image: "https://www.youtube.com/embed/Hqt1D01T4ZI",
-    cover: "../../../assets/cover/img3.png",
+    cover: "../../../assets/cover/img3.jpg",
     descriptionFR: "JustRun - Nouvelle musique",
     descriptionEN: "JustRun - New music",
   },
