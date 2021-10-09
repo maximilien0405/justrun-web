ce87db4b221a2f37f9045c9ea9da598f0611d2bc
maximilien0405 <maximilien.zimmermann@ik.me>
2021-10-09 12:14:42 +0200

    remove antoine from list

--- src/app/profile-list.ts
+++ src/app/profile-list.ts
@@ -21,36 +21,6 @@ export const PROFILE: Profile[] = [
       socialThreeLink: "https://www.behance.net/max-zim",
       socialThreeImg: "../assets/social/behance.svg",
     },
-    /*{
-      userName: "Quentin F.",
-      roleFR: "Chef de projet",
-      roleEN: "Project Manager",
-      pdp: "../../../assets/img/pdp/quentin.jpeg",
-      group: "project-management",
-      presentationFR: "AKA Carcaroux, actuellement à la recherche d'un concurrent à sa taille sur Mario Kart.",
-      presentationEN: "AKA Carcaroux, currently looking for a worthy competitor on Mario Kart.",
-      socialOneLink: "https://www.linkedin.com/in/quentin-fert-6876b2164/",
-      socialOneImg: "../assets/social/linkedin.svg",
-      socialTwoLink: "https://www.arcadia-corporation.com/community/carcaroux",
-      socialTwoImg: "../assets/social/website.svg",
-      socialThreeLink: "mailto:quentin.fert.pro@gmail.com",
-      socialThreeImg: "../assets/social/mail.svg",
-    },*/
-    {
-      userName: "Antoine R.",
-      roleFR: "Contributeur",
-      roleEN: "Contributor",
-      pdp: "../../../assets/img/pdp/antoine.jpg",
-      group: "project-management",
-      presentationFR: "Grâce à lui vous allez pouvoir terminer un projet de qualité tout en prenant vos pauses cafés.",
-      presentationEN: "Thanks to him you will be able to finish a quality project while taking your coffee breaks.",
-      socialOneLink: "https://www.linkedin.com/in/antoine-rigitano-038862b3/",
-      socialOneImg: "../assets/social/linkedin.svg",
-      socialTwoLink: "https://www.behance.net/arigitano",
-      socialTwoImg: "../assets/social/behance.svg",
-      socialThreeLink: "",
-      socialThreeImg: "",
-    },
 
     /* --- CREATIVE TEAM ---- */
 
